1635ea24f35a383bbce1319462d8e5c4575107e6
Admin <jaipkamat@gmail.com>
2021-01-01 16:12:28 -0800

    code formatting and passing hardcoded location

--- api.js
+++ api.js
@@ -1,24 +1,27 @@
 const axios = require('axios');
 
-const scryfallAutocomplete = async title => {
+const scryfallAutocomplete = async (title) => {
     try {
-        const { data } = await axios.get('https://api.scryfall.com/cards/autocomplete', {
-            params: { q: decodeURI(title) }
-        });
+        const { data } = await axios.get(
+            'https://api.scryfall.com/cards/autocomplete',
+            {
+                params: { q: decodeURI(title) },
+            }
+        );
 
         return data.data; // Scryfall's res is data, on top of previous data...
     } catch (err) {
         console.log(err);
     }
-}
+};
 
-const getCardsFromChcollector = async title => {
+const getCardsFromChcollector = async (title) => {
     const { data } = await axios.get(process.env.FETCH_CARDS_TEST, {
-        params: { matchInStock: true, title: title }
-    })
+        params: { matchInStock: true, title: title, location: 'ch1' },
+    });
 
     return data;
-}
+};
 
 module.exports.scryfallAutocomplete = scryfallAutocomplete;
 module.exports.getCardsFromChcollector = getCardsFromChcollector;
